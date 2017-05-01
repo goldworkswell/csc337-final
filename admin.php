c5482e4144ecb530d0eb13a39dc6b296d51107e4
Andrew Guy <andrewguy@email.arizona.edu>
2017-05-01 13:39:30 -0700

    adslkjds
    adssd

---
+++ admin.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+    <link rel = "stylesheet" href = "style1.css" type = "text/css">
+    <link rel = "stylesheet" href = "style.css" type = "text/css">
+     <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+<body>
+    
+    
+    <a href = "index.php"><input type = "submit" value = "Log me out"></a>
+    
+    
+
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Dollar Shop</a>
+    </div>
+      <div class="navbar-header">
+      <p class="navbar-brand" id = "lin">Username: <?=$_SESSION['user']?></p>
+    </div>
+      <ul class="nav navbar-nav navbar-right">
+      <a href = "index.php"><input type = "submit" value = "Log me out" class = "btn btn-danger navbar- btn"></a>
+      </ul>
+      
+      
+ 
+      
+      
+      
+  </div>
+</nav>
+    </body>
+</html>
