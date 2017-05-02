fd9cbba09294333d937dd0294b12340c664ae74a
goldworkswell <mgold1@email.arizona.edu>
2017-05-02 11:30:00 -0700

    changed the inventory to admin

--- shopping.sql
+++ shopping.sql
@@ -9,11 +9,11 @@ CREATE TABLE users (
     PRIMARY KEY (id),
     UNIQUE KEY (username)
 );
-CREATE TABLE inventory (
-    shopper_id int(11) NOT NULL AUTO_INCREMENT,
-    item varchar(64) NOT NULL DEFAULT '',
-    quantity int(11),
-    PRIMARY KEY (shopper_id)
+CREATE TABLE admin (
+    user_id int(11) NOT NULL,
+    item_id varchar(64) NOT NULL,
+    quant int(11),
+    username varchar(64) NOT NULL
 );
 CREATE TABLE cart (
     user_id int(11) NOT NULL,
